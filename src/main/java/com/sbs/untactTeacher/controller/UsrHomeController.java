8361903bd63b7488dd4dc2c4a0f42b8e8ccf6a04
Dong1414 <70949409+Dong1414@users.noreply.github.com>
2021-03-23 12:04:31 +0900

    db

---
+++ src/main/java/com/sbs/untactTeacher/controller/UsrHomeController.java
@@ -0,0 +1,14 @@
+package com.sbs.untactTeacher.controller;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.sbs.untactTeacher.util.Util;
+
+@Controller
+public class UsrHomeController {
+
+}
